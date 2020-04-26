307ca633aeb389796db0ecd49303f3343459fa49
Lauren Conley <laurenashley.mail@gmail.com>
2020-04-26 15:16:42 -0600

    add messages and update index view to display messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "hey there!",
+    user: 'Lauren',
+    added: new Date()
+  },
+  {
+    text: "Hello World",
+    user: 'Drew',
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board', messages: messages });
 });
 
 module.exports = router;
